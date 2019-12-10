5ad3e20ef15c68d2c62f3b64a1cd2351328c1acd
ivanfslee <ivanfslee@gmail.com>
2019-12-09 18:58:48 -0800

    Added edit/delete stream buttons and create stream button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component { //class based component because we need access to lifecycle method 
     componentDidMount() {
         this.props.fetchStreams();
     }
-
+    renderAdmin(stream) { //controls whether edit/delete buttons show up 
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            );
+        }
+    }
     renderList() {
         return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
                         <div className="description">
                             {stream.description}
                         </div>
-                    </div>
+                    </div>                  
                 </div>
             )
         })
+    }
+
+    renderCreate() { //render create button 
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{textAlign: 'right'}}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
     }
 
     render() {
@@ -29,13 +56,18 @@ class StreamList extends React.Component { //class based component because we ne
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         );
     }
 };
 
 const mapStateToProps = state => {
-    return { streams: Object.values(state.streams)} //take values in state.streams obj and put into array using Object.values
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    } //take values in state.streams obj and put into array using Object.values
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
