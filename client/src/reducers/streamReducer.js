9ca62be397e9efd361fef772547278eb7ba5b246
ivanfslee <ivanfslee@gmail.com>
2019-12-09 16:25:07 -0800

    Added streamReducer to handle adding/editing streams

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import {
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAMS :
+            //merge current streams with new stream
+            return {...state, ..._.mapKeys(action.payload, 'id') } //second arg lodash method mapKeys - creates an obj with id number as the key and the action.payload as the value
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload } //bracket syntax is key interpolation es6
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload }
+        case EDIT_STREAM:
+            return { ...state, [action.payload.is]: action.payload }
+        case DELETE_STREAM:
+            return _.omit(state, action.payload); //omit method from lodash creates a new obj with all properties from original state except action.payload property (which is an id)
+        default: 
+            return state;
+    }
+};
